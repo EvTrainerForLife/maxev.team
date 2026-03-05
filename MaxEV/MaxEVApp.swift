5c76f12f6da8e27c07e8f931d60ee14b89edd154
Tony Zepeda <tzepeda4@gmail.com>
2026-03-04 20:14:07 -0600

    Initial Commit

---
+++ MaxEV/MaxEVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxEVApp.swift
+//  MaxEV
+//
+//  Created by Tony Zepeda on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MaxEVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
